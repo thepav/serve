b35241b4dceeae71b2b8df09c249640ee31228bd
Pav <pav920@gmail.com>
2014-07-03 18:01:04 -0400

    trying to spawn simplehttpserver on heroku

--- serve.py
+++ serve.py
@@ -9,7 +9,7 @@ def hello():
 	#convert hash to username
 	# username = getUsername(userHash)
     #    return render_template('process.html', username=username)
- 	stuff = os.popen("ls -l").read()
+ 	stuff = os.popen("python -m simplehttpserver_test/server.py 7000").read()
 	return stuff
 
 def getUsername(userHash):
---
+++ simplehttpserver_test/server.py
@@ -0,0 +1,14 @@
+import SimpleHTTPServer
+import SocketServer
+
+class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/yolo.png'
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+Handler = MyRequestHandler
+server = SocketServer.TCPServer(('0.0.0.0', 80), Handler)
+
+server.serve_forever()
+
