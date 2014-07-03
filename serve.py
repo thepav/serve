233599541ee7aec9507010c4815a25b55d186c13
Pav <pav920@gmail.com>
2014-07-03 17:14:29 -0400

    trying system call

--- serve.py
+++ serve.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask import render_template
+ 	from subprocess import call
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/<userHash>")
+def hello(userHash=None):
+	#convert hash to username
+	# username = getUsername(userHash)
+ #    return render_template('process.html', username=username)
+	return call(["ls", "-l"])
+
+def getUsername(userHash):
+	return 'Pav'
 
 if __name__ == "__main__":
     app.debug = True
