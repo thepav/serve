a5506b909b4d0c54eb2ca0c8f9588dae53d2dcfe
Pav <pav920@gmail.com>
2014-07-04 02:34:57 -0400

    video background

---
+++ serve.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+import os
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	#convert hash to username
+	# username = getUsername(userHash)
+	# subprocess.call(['python server.py &'], shell=True)
+ # 	print os.popen("jobs -l").read()
+	stuff2 = os.popen("ps -fA | grep python").read().replace('\n','<br>')
+ 	stuff2 = stuff2 + str('<br><br><br>') + os.popen('ls').read()
+	return render_template('index.html', stuff=stuff2)
+ 	
+	#print stuff2
+ 	# python -m simplehttpserver_test/server.py 80
+	return stuff2
+
+def getUsername(userHash):
+	return 'Pav'
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
