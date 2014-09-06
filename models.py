b6a96024121ae7870d5d4471fb863095b49ed927
Pav <pav920@gmail.com>
2014-09-06 04:33:45 -0400

    added models.py

--- models.py
+++ models.py
@@ -2,5 +2,5 @@ from serve import db
 
 class User(db.Document):
     email = db.StringField(required=True)
-    password = db.StringField(max_length=50, required=true)
+    password = db.StringField(max_length=50, required=True)
     name = db.StringField(max_length=50)
