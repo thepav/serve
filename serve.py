dbd5293fcb779c2879a65dcfdeda5e5043cb9822
Joshua Morton <joshua.morton13@gmail.com>
2014-09-06 13:16:48 -0400

    fixed a small bug

--- serve.py
+++ serve.py
@@ -108,7 +108,7 @@ def newFunc():
 		appdoe = App.objects(id=AppId)[0]
 		appdoe.numberFunc += 1
 
-		function = Function( AppId=AppId, name=name, code=code) # STILL NEED TO WORK OUT DEPENDENCIES!
+		function = Function(AppId=AppId, userId=userId, name=name, code=code) # STILL NEED TO WORK OUT DEPENDENCIES!
 		function.save()
 
 		return redirect(url_for('gallery', pk = userId))
