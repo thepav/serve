ff6c0ea7955d1c6b627514d329a41138075e0971
root <root@ip-172-31-36-50.us-west-2.compute.internal>
2014-09-06 06:43:20 +0000

    mongo'ed doe, start of user auth

--- serve.py
+++ serve.py
@@ -1,9 +1,21 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for, redirect, abort, request
 import os
 import subprocess
+from flask.ext.mongoengine import MongoEngine
+
+db = MongoEngine()
 
 serve = Flask(__name__)
+#serve.config.from_pyfile('the-config.cfg')
+serve.config['MONGODB_SETTINGS'] = {
+    'db': 'test',
+    'host': 'localhost',
+    'port': 27017
+}
+
+db.init_app(serve)
+
 
 @serve.route("/")
 def hello():
@@ -15,8 +27,25 @@ def hello():
  	stuff2 = stuff2 + str('<br><br><br>') + os.popen('ls').read()
 	return render_template('index.html', stuff=stuff2)
 
-def getUsername(userHash):
-	return 'Pav'
+@serve.route("/auth/", methods='POST')
+def auth():
+	if request.method == 'POST':
+        	user = request.form['user']
+		password = request.form['pass']
+		
+    	else:
+		return render_template('login_failed.html');
+@serve.route('/login/')
+def login():
+	return render_template('login.html')
+
+@serve.route('/signup/')
+def signup():
+        return render_template('signup.html')
+
+
+
+
 
 if __name__ == "__main__":
     serve.debug = True
